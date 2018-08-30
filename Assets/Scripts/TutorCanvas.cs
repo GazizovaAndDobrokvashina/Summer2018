c2c26aed9489b0de6b1f7eed245bb425972bf9a3
Elina Gazizova <lady.gazizova2011@yandex.ru>
2018-08-30 23:07:00 +0300

    no message

--- Assets/Scripts/TutorCanvas.cs
+++ Assets/Scripts/TutorCanvas.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.U2D;
 using UnityEngine.UI;
 
 public class TutorCanvas : MonoBehaviour
@@ -9,15 +10,19 @@ public class TutorCanvas : MonoBehaviour
     public GameObject PicturesDialogsWindows;
     public GameObject FoxIcons;
     public GameObject CatIcons;
+    private Image catImage;
+    public Image poster;
+    public SpriteAtlas atlas;
     public Text FoxText;
     public Text CatText;
     private Replica _replica;
     private string _endOfPhrase;
     public GameObject tutorButton;
-    [SerializeField]private UnityEvent _wallUnblocked;
 
     void Start()
     {
+
+        catImage = CatIcons.GetComponentInChildren<Image>();
         _endOfPhrase = "\n(Кликните в любом месте, чтобы продолжить)";
         //если обучение ещё не пройдено, то начинаем обучение
         if (PlayerPrefs.GetInt("TutorFinished") == 0)
@@ -36,6 +41,19 @@ public class TutorCanvas : MonoBehaviour
     {
         NextReplica();
         Time.timeScale = 0;
+
+        if (_replica.Id == 1)
+        {
+            catImage.sprite = atlas.GetSprite("AvaCat2");
+            poster.sprite = atlas.GetSprite("Poster2");
+        }
+        
+        if (_replica.Id == 2)
+        {
+            catImage.sprite = atlas.GetSprite("AvaCat3");
+            poster.sprite = atlas.GetSprite("Poster3");
+        }
+        
         if (_replica.Id == 7 || _replica.Id == 14 || _replica.Id == 16)
         {
             Time.timeScale = 1;
